c2359a822097cc4aeec8005b43108513b9948226
abhav <abhav.thakur2018@vitstudent.ac.in>
2020-07-08 21:09:29 +0530

    users

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,23 @@
 const socket = io();
 const chatFrom = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+//get username and room from url
+const {username,room} = Qs.parse(location.search,{
+  ignoreQueryPrefix:true
+});
+
+//join chat room
+socket.emit('joinRoom',{username, room});
 
 
-
-socket.on('message',message => {
+//message from server
+socket.on('message', message => {
   console.log(message);
+  outputMessage(message);
+
+  //Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;;
 });
 
 //Message submit
@@ -12,6 +25,24 @@ socket.on('message',message => {
 chatFrom.addEventListener('submit', e =>{
   e.preventDefault();
   
+  //get message text
   const msg = e.target.elements.msg.value;
-  console.log(msg);
+  
+  //emit message to server
+  socket.emit('chatMessage',msg);
+
+  //clear input or adding new value to input line
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 });
+
+//output message to DOM
+function outputMessage(message){
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML= `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+   ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -2,25 +2,40 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
-
+const formatMessage = require('./utils/messages');
+const { userJoin , getCurrentUser } = require('./utils/user');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const botName = 'chatroom bot';
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 //run when client connects
 io.on('connection',socket => {
-    console.log("new ws connection..");
-    //welcome the current user
-    socket.emit('message','welcome to chatroom');
 
-    //broadcast when auser connects
-    socket.broadcast.emit('message','A new user has joinned the chat');
-    
+    socket.on('joinRoom',({ username , room}) =>{
+
+        const user = userJoin(socket.id,username, room);
+        socket.join(user.room );
+
+        console.log("new ws connection..");
+    //welcome the current user
+    socket.emit('message',formatMessage(botName,'welcome to chatroom'));
+
+    //broadcast when a user connects
+    socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} has joinned the chat`));
+    });
+
     //when a user disconnect
     socket.on('disconnect',()=>{
-        io.emit('message', 'a user has left the chat');
+        io.emit('message', formatMessage(botName,`User has left the chat`));
+    });
+    //listen for the chat message
+    socket.on('chatMessage', msg =>{
+        const user = getCurrentUser(socket.id);
+       io.to(user.room).emit('message',formatMessage(user.username, msg));
     });
 });
 
