6148a38daf98c1e57df68d5bac6c19e67cce08c2
abhav <abhav.thakur2018@vitstudent.ac.in>
2020-07-09 18:24:41 +0530

    password system

---
+++ public/js/pass.js
@@ -0,0 +1,9 @@
+var password = prompt("Enter in the password");
+        if (password=="1234") {
+            document.getElementById("msg").value = "test";  
+        }
+        else 
+        {
+         
+          location = "error.html"
+        }
